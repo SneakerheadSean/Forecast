5647b721ae1abdf66e8d344265a10842253ae209
Sean Hughes <sneakerheadsean@gmail.com>
2022-08-08 11:38:16 -0400

    Initial Commit

---
+++ Forecast/ForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastApp.swift
+//  Forecast
+//
+//  Created by Sean Hughes on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
